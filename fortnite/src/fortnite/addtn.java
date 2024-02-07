d47adca6e393f7a2ed899ebe8e15867d9da61492
SAENRO GONSALVES <SAENRO GONSALVES@LAPTOP-SAEN>
2024-02-07 22:08:30 +0530

    commiting 1

---
+++ fortnite/src/fortnite/addtn.java
@@ -0,0 +1,21 @@
+package fortnite;
+
+import java.util.Scanner;
+
+public class addtn {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x,y;
+		
+		Scanner scan = new Scanner(System.in);
+		x=scan.nextInt();
+		y=scan.nextInt();
+
+		int add=x+y;
+		System.out.println("Answer: "+add);
+
+	}
+
+}
---
+++ fortnite/src/fortnite/dan.java
@@ -0,0 +1,12 @@
+package fortnite;
+
+public class dan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello wpord");
+
+
+	}
+
+}
